964177ac7505f26d82f25a1e9782618e332fb902
vwik20 <vwik20@student.oulu.fi>
2023-02-18 18:31:03 +0200

    muutokset

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -8,17 +8,46 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    s = s.ljust(1000, 'a')
+    
+    for c in s:
+         if c.lower() in ["+", "å", "ä", "ö"]:
+            raise ValueError
     for c in s:
         if c.isalpha():
             if c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
+            elif c.isupper():
+                 c=c.lower()
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
 
-    return crypted
+        
+    return crypted[:origlen]
 
 def decode(s):
-    return s
+   # if not isinstance(s,str):
+    #   raise TypeError
+    origlen = len(s)
+    crypted = ""
+    digitmapping = dict(zip('!"#€%&/()=1234567890','1234567890!"#€%&/()='))
+   # if len(s) > 1000:
+    #    raise ValueError
+    s = s.ljust(1000, 'a')
+   # for c in s:
+        # if c.lower() in ["+", "å", "ä", "ö"]:
+         #    raise ValueError
+    for c in s:
+        if c.isalpha():
+            if c.islower():
+                c=c.upper()
+            # Rot13 the character for maximum security
+            elif c.isupper():
+                 c=c.lower()
+            crypted+=codecs.decode(c,'rot13')
+        elif c in digitmapping:
+          crypted+=digitmapping[c]
 
+    return crypted[:origlen]
--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -29,20 +29,21 @@ def test_encode(test_input, expected):
 
 
 @pytest.mark.parametrize("test_input", [
-    '123', '!"#','abc'])
+    '123', '!"#','abc', 'ABC'])
 def test_encode_decode(test_input):
     '''Verify that decoding an encoded string returns original string'''
     assert(mycrypt.decode(mycrypt.encode(test_input))) == test_input
 
 
-@pytest.mark.parametrize("invalid_input", ['+','åäö'])
+@pytest.mark.parametrize("invalid_input", ['+','åäö', ('a'*1001)])
 def test_invalid_char(invalid_input):
     '''Invalid characters should result in ValueError'''
     with pytest.raises(ValueError):
         mycrypt.encode(invalid_input)
 
+        
 
-@pytest.mark.parametrize("invalid_input", [])
+@pytest.mark.parametrize("invalid_input", [2])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
